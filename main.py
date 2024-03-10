bf1ebaf4bdddfc016f3b20a235825e9d1424aa47
Vincent <vincent.dagoury@gmail.com>
2024-03-10 21:00:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+# Import typings for python
+import os
+
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import Response
+from bson import ObjectId
+import motor.motor_asyncio
+from dotenv import load_dotenv
+from models.Student import StudentCollection, StudentModel, UpdateStudentModel
+
+# Import the MONGODB_URL from .env file
+load_dotenv()
+client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
+db = client.get_database("test")
+student_collection = db.get_collection("students")
+
+
+app = FastAPI(
+    title="Student Course API",
+    summary="A sample application showing how to use FastAPI to add a ReST API to a MongoDB collection.",
+)
+
+@app.get(
+    "/students/",
+    response_description="List all students",
+    response_model=StudentCollection,
+    response_model_by_alias=False,
+)
+async def list_students():
+    """
+    List all of the student data in the database.
+
+    The response is unpaginated and limited to 1000 results.
+    """
+    return StudentCollection(students=await student_collection.find().to_list(1000))
+
+@app.post(
+    "/students/",
+    response_description="Add new student",
+    response_model=StudentModel,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
+async def create_student(student: StudentModel = Body(...)):
+    """
+    Insert a new student record.
+
+    A unique `id` will be created and provided in the response.
+    """
+    new_student = await student_collection.insert_one(
+        student.model_dump(by_alias=True, exclude=["id"])
+    )
+    created_student = await student_collection.find_one(
+        {"_id": new_student.inserted_id}
+    )
+    return created_student
+
+@app.get(
+    "/students/{id}",
+    response_description="Get a single student",
+    response_model=StudentModel,
+    response_model_by_alias=False,
+)
+async def show_student(id: str):
+    """
+    Get the record for a specific student, looked up by `id`.
+    """
+    if (
+        student := await student_collection.find_one({"_id": ObjectId(id)})
+    ) is not None:
+        return student
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+@app.put(
+    "/students/{id}",
+    response_description="Update a student",
+    response_model=StudentModel,
+    response_model_by_alias=False,
+)
+async def update_student(id: str, student: UpdateStudentModel = Body(...)):
+    """
+    Update individual fields of an existing student record.
+
+    Only the provided fields will be updated.
+    Any missing or `null` fields will be ignored.
+    """
+    student = {
+        k: v for k, v in student.model_dump(by_alias=True).items() if v is not None
+    }
+
+    if len(student) >= 1:
+        update_result = await student_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": student},
+            return_document=ReturnDocument.AFTER,
+        )
+        if update_result is not None:
+            return update_result
+        else:
+            raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+    # The update is empty, but we should still return the matching document:
+    if (existing_student := await student_collection.find_one({"_id": id})) is not None:
+        return existing_student
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+
+@app.delete("/students/{id}", response_description="Delete a student")
+async def delete_student(id: str):
+    """
+    Remove a single student record from the database.
+    """
+    delete_result = await student_collection.delete_one({"_id": ObjectId(id)})
+
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
